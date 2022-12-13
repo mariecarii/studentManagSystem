af626395e56cb5f1f3f17760cbe5756e05159083
Marie Cari <mcari@nyc.yearup.org>
2022-12-13 17:08:52 -0500

    ask user how many new students to add to database

--- studentManagApp.java
+++ studentManagApp.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class studentManagApp {
 
     public static void main(String[] args) {
-        Student stud1 = new Student();
-        stud1.showInfo();
         //ask how many students
+        System.out.println("Enter number of new students: ");
+        Scanner scan = new Scanner(System.in);
+        int numOfStudents = scan.nextInt();
+        Student [] students = new Student[numOfStudents];
 
         //create # of students
+        for (int i=0; i<numOfStudents; i++) {
+            students[i] = new Student();
+            students[i].showInfo();
+            System.out.println();
+
+        }
+
+
+
     }
 }
