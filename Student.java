9a2b078d132e31011e149af45e9536c47e7a874c
Marie Cari <mcari@nyc.yearup.org>
2022-12-12 23:00:12 -0500

    started constructor that takes in user input for name and year

--- Student.java
+++ Student.java
@@ -1,14 +1,32 @@
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int studentID;
     private String courses;
     private int balance;
     private int costPerCourse = 600;
 
     // constructor to enter name and year
+    public Student() {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter first name: ");
+        this.firstName = scan.nextLine();
+
+        System.out.println("Enter last name: ");
+        this.lastName = scan.nextLine();
+
+        System.out.println("Enter year: ");
+        this.gradeYear = scan.nextInt();
+        System.out.println(firstName + lastName + gradeYear);
+
+
+
+
+    }
     // generate a 5 digit ID
 
     // enroll in courses
--- studentManagApp.java
+++ studentManagApp.java
@@ -1,6 +1,8 @@
 public class studentManagApp {
 
     public static void main(String[] args) {
+        Student stud1 = new Student();
+
         //ask how many students
 
         //create # of students
