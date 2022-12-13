ad3ba78b28ccfdfd0348dc94340c5280f9b398bc
Marie Cari <mcari@nyc.yearup.org>
2022-12-12 22:51:10 -0500

    created outline/puesdo code

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentID;
+    private String courses;
+    private int balance;
+    private int costPerCourse = 600;
+
+    // constructor to enter name and year
+
+    // generate a 5 digit ID
+
+    // enroll in courses
+
+    // view balance and pay tuition
+
+    // shows status/info
+}
---
+++ studentManagApp.java
@@ -0,0 +1,8 @@
+public class studentManagApp {
+
+    public static void main(String[] args) {
+        //ask how many students
+
+        //create # of students
+    }
+}
